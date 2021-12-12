1cbe9c55bd5f01abf5959898d7465a5ce8b6ea8d
Eamonn Duffy <Eamonn@Duffy.global>
2021-12-12 23:42:35 +0000

    Hiding Rapture Sites and Éamonn Anthony Duffy for now. Can enable in Link by "?show=All". I need to address my concerns with these sites moving forward.

--- Duffy/Pages/V1/Link.cshtml.cs
+++ Duffy/Pages/V1/Link.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace Duffy.Pages.V1
 {
@@ -12,6 +13,7 @@ namespace Duffy.Pages.V1
             public string Name { get; set; }
             public string Uri { get; set; }
             public bool HttpsAvailable { get; set; }
+            public bool ShowAlways { get; set; }
         };
 
         // MetaClass Attributes.
@@ -22,49 +24,56 @@ namespace Duffy.Pages.V1
                 Reference = "R1000",
                 Name = "Dublin City University (DCU, née NIHE)",
                 Uri = "//www.Dcu.ie/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R1001",
                 Name = "Duffy Web Site",
                 Uri = "//www.Duffy.global/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R1002",
                 Name = "Éamonn Anthony Duffy Web Site",
                 Uri = "//www.Duffy.global/Éamonn Anthony Duffy/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = false
             },
             new LinkInfo
             {
                 Reference = "R2000",
                 Name = "EVE",
                 Uri = "//www.Eve.ie/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R2001",
                 Name = "GHIS",
                 Uri = "//www.Ghis.eu/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R2002",
                 Name = "GHIS Student Portfolio",
                 Uri = "//Web.Ghis.eu/Portfolio_EAD.html",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R2003",
                 Name = "GHIS Web Design",
                 Uri = "//Web.Ghis.eu/Index.html",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
@@ -73,7 +82,8 @@ namespace Duffy.Pages.V1
                 // Previously:
                 //  //www.CareFusion.com/
                 Uri = "//www.Natus.com/",
-                HttpsAvailable = false
+                HttpsAvailable = false,
+                ShowAlways = true
             },
             new LinkInfo
             {
@@ -82,42 +92,48 @@ namespace Duffy.Pages.V1
                 // Previously:
                 //  //www.Sony.co.uk/Biz
                 Uri = "//www.Sony.co.uk/Pro",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3002",
                 Name = "Kismet Studios",
                 Uri = "//www.Eadent.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3003",
                 Name = "GoldPlay UK",
                 Uri = "//www.Eadent.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3004",
                 Name = "MuseGaming",
                 Uri = "//www.Eadent.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3005",
                 Name = "Eadent",
                 Uri = "//www.Eadent.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3006",
                 Name = "MMI Research",
                 Uri = "//www.Cobham.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
@@ -126,7 +142,8 @@ namespace Duffy.Pages.V1
                 //  6-Jun-2010: Redirects to: http://www.lexisnexis.org.uk/practiceandproductivitymanagement/
                 // 16-Nov-2016: Redirects to: http://www.lexisnexis-es.co.uk/
                 Uri = "//www.Axxia.com/",
-                HttpsAvailable = false
+                HttpsAvailable = false,
+                ShowAlways = true
             },
             new LinkInfo
             {
@@ -136,35 +153,41 @@ namespace Duffy.Pages.V1
                 // 16-Nov-2016: Redirects to: https://www.fiserv.com/index.aspx
                 //  3-Sep-2018: www.CheckFree.com no longer appears to redirect.
                 Uri = "//www.fiserv.com/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R3009",
                 Name = "Payzone",
                 Uri = "//www.Payzone.ie/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = true
             },
             new LinkInfo
             {
                 Reference = "R4000",
                 Name = "Rapture Web Site",
                 Uri = "//www.Rapture.global/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = false
             },
             new LinkInfo
             {
                 Reference = "R4001",
                 Name = "Rapture Therapy Web Site",
                 Uri = "//Therapy.Rapture.global/",
-                HttpsAvailable = true
+                HttpsAvailable = true,
+                ShowAlways = false
             },
         };
 
         // Model Properties.
         public LinkInfo[] LinkInfoList { get; set; }
 
-        public IActionResult OnGet(string reference)
+        public bool ShowAll { get; set; }
+
+        public IActionResult OnGet(string reference, string show = null)
         {
             IActionResult actionResult = null;
             
@@ -185,6 +208,9 @@ namespace Duffy.Pages.V1
                     }
                 }
             }
+
+            if (string.Equals(show, "All", StringComparison.OrdinalIgnoreCase))
+                ShowAll = true;
 
             if (actionResult == null)
             {
